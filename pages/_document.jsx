92be304eeb9c3caac5287a4a42c4b172b9468ecb
Icaro8 <soprafazermissal@gmail.com>
2022-02-03 16:00:00 -0300

    feat/init

---
+++ pages/_document.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Html, Head, NextScript, Main } from "next/document";
+
+export default function _document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Source+Serif+Pro:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
